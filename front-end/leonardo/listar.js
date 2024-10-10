fe7b87e880ab07f80f8f2fc685e6d0fc84458c85
leovlrw <leonardovalerio89@gmail.com>
2024-10-10 00:40:45 -0300

    Alterações no front 1.01

---
+++ front-end/leonardo/listar.js
@@ -0,0 +1,15 @@
+const url = ''; //aqui tenho que inserir a url do sistema 
+
+        const data = {
+            //no caso aqui eu iria utilizar se eu quiser enviar um dado
+        }
+
+        const fazerRequisicao = async () => {
+            // try{
+            //     const response = await axios.post(url, data);//aqui eu uso para ver  a resposta do servidor e ver se foi feito corretamente
+            //     console.log('e pra funcionar',response.data);
+            // }catch (error) {
+            //     console.log('nao funciou ',error);
+            // }
+        };
+        fazerRequisicao; //aqui eu chamo da mesma forma que se faz no react 
