205da56f882873314ed9b5fad28fab3202f4b101
joaoputini <2015.silvajoao@gmail.com>
2024-10-14 11:47:01 -0300

    criando a funçao adicionar agentes

--- front-end/leonardo/listar.js
+++ front-end/leonardo/listar.js
@@ -36,7 +36,33 @@ async function listar() {
         console.error('Erro ao buscar ocorrências:', error);
     }
 }
+async function enviarDados(dados) {
+    try {
+        const resposta = await fetch('http://localhost:8080/agentes/nao lembro onde ta ', { 
+            method: 'POST',  // post server para adicionar os dados se quiser editar dados existentes ja eu adiciono o put 
+            headers: { //objeto utilizado para definir cabeçalho 
+                'Content-Type': 'application/json'  //informar ao servidor que esta em formato json
+            },
+            body: JSON.stringify(dados) //enviando os dados que Desejamos e convertendo ele para json
+        });
 
+        // Verifica se a resposta foi bem-sucedida
+        if (resposta.ok) {
+            const resultado = await resposta.json(); // Converte a resposta para JSON
+            console.log('Dados enviados com sucesso:', resultado);
+        } else {
+            console.error('Erro ao enviar dados:', resposta.status, resposta.statusText);
+        }
+    } catch (erro) {
+        console.error('Erro na requisição:', erro);
+    }
+}
+
+const dadosParaEnviar = {
+    agente: {nome: "Nome do Agente" } // enviar o nome do agente pro banco
+};
+// Chama a função para enviar os dados
+enviarDados(dadosParaEnviar);
 // Chama o método listar ao carregar a página
 document.addEventListener('DOMContentLoaded', () => {
     listar();
