f68a595f2a77fe438edbfd4e8d0b2ff9ee568e65
LuisRohrer <luishenrique_018@hotmail.com>
2024-12-06 21:27:12 -0300

    correções nos modais e estilização em tal

--- codigo correto para edição/geral/lista.js
+++ codigo correto para edição/geral/lista.js
@@ -1,15 +1,15 @@
-getAllOcorrencias();
+getAllOcorrencias()
 async function getAllOcorrencias() {
-  const response = await axios.get("http://127.0.0.1:8080/ocorrencia/listar");
+  const response = await axios.get('http://10.110.12.40:8080/ocorrencia/listar')
 
-  const dados = response.data;
+  const dados = response.data
 
-  createCardOcorrencia(dados);
+  createCardOcorrencia(dados)
 }
 
 function createCardOcorrencia(dados) {
-  const containCards = document.querySelector(".ocorrencias-div");
-  containCards.innerHTML = "";
+  const containCards = document.querySelector('.ocorrencias-div')
+  containCards.innerHTML = ''
 
   dados.forEach((ocorrencia) => {
     const {
@@ -19,8 +19,7 @@ function createCardOcorrencia(dados) {
       dataCriacao,
       dataAtualizacao,
       agente,
-    } = ocorrencia;
-
+    } = ocorrencia
 
     containCards.innerHTML += `
       <div class="ocorrencia" data-id="${id}">
@@ -28,8 +27,8 @@ function createCardOcorrencia(dados) {
           <h3>Ocorrência  ${id}</h3>
           <div class="contain-indicator-status-ocorrencia">
             <div class="indicador-status-ocorrencia ${selectColorStatusOcorrencia(
-      statusOcorrencia
-    )}"></div>
+              statusOcorrencia
+            )}"></div>
           </div>
         </div>
         <p>Status: ${statusOcorrencia}</p>
@@ -41,199 +40,216 @@ function createCardOcorrencia(dados) {
         <button class="btn-ocorrencia-delete">Deletar</button>
         <button class="btn-ocorrencia-status">Atualizar Status</button>
       </div>
-    `;
-  });
+    `
+  })
 }
 
-document.getElementById("form-ocorrencia").addEventListener("submit", async function (event) {
-  event.preventDefault(); // Evita o recarregamento da página
+document
+  .getElementById('form-ocorrencia')
+  .addEventListener('submit', async function (event) {
+    event.preventDefault() // Evita o recarregamento da página
 
-  const descricao = document.querySelector("#descricao").value;
-  const statusOcorrencia = document.querySelector("#statusOcorrencia").value;
-  const agenteId = document.getElementById("agenteSelecionado").dataset.id;
+    const descricao = document.querySelector('#descricao').value
+    const statusOcorrencia = document.querySelector('#statusOcorrencia').value
+    const agenteId = document
+      .querySelector('.btn-agent-Ocorrencia')
+      .getAttribute('data-id')
 
-  if (!agenteId) {
-    alert("Por favor, selecione um agente.");
-    return;
-  }
+    if (!agenteId) {
+      alert('Por favor, selecione um agente.')
+      return
+    }
 
-  await axios.post("http://127.0.0.1:8080/ocorrencia/adicionar", {
-    observacoes: descricao,
-    statusOcorrencia: statusOcorrencia,
-    agente: { id: agenteId },
-  });
+    await axios.post('http://10.110.12.40:8080/ocorrencia/adicionar', {
+      observacoes: descricao,
+      statusOcorrencia: statusOcorrencia,
+      agente: { id: agenteId },
+    })
 
-  alert("Ocorrência adicionada com sucesso!");
-  getAllOcorrencias(); // Atualiza a lista de ocorrências
-  openOcorrenciaModal(); // Fecha o modal de ocorrências
-});
-
-
+    alert('Ocorrência adicionada com sucesso!')
+    getAllOcorrencias() // Atualiza a lista de ocorrências
+    openOcorrenciaModal() // Fecha o modal de ocorrências
+  })
 
 function selectColorStatusOcorrencia(statusOcorrencia) {
-  if (statusOcorrencia == "Pendente") {
-    return "background-orange-indicator";
-  } else if (statusOcorrencia == "Concluído") {
-    return "background-green-indicator";
-  } else if (statusOcorrencia == "Em Andamento") {
-    return "background-yellow-indicator";
+  if (statusOcorrencia == 'Pendente') {
+    return 'background-orange-indicator'
+  } else if (statusOcorrencia == 'Concluído') {
+    return 'background-green-indicator'
+  } else if (statusOcorrencia == 'Em Andamento') {
+    return 'background-yellow-indicator'
   }
 }
 
-
 function inputAgente() {
-  const nomeAgente = document.querySelector("#nomeAgente").value;
-  const cpfAgente = document.querySelector("#cpfAgente").value;
-  const rgAgente = document.querySelector("#rgAgente").value;
-  const departamentoAgente = document.querySelector("#DepartamentoAgente").value;
+  const nomeAgente = document.querySelector('#nomeAgente').value
+  const cpfAgente = document.querySelector('#cpfAgente').value
+  const rgAgente = document.querySelector('#rgAgente').value
+  const departamentoAgente = document.querySelector('#DepartamentoAgente').value
 
-  adicionarAgente(nomeAgente, cpfAgente, rgAgente, departamentoAgente);
+  adicionarAgente(nomeAgente, cpfAgente, rgAgente, departamentoAgente)
 }
 
-async function adicionarAgente(nomeAgente, cpfAgente, rgAgente, departamentoAgente) {
-
-
-  const resposta = await axios.post("http://localhost:8080/pessoa/adicionar", {
+async function adicionarAgente(
+  nomeAgente,
+  cpfAgente,
+  rgAgente,
+  departamentoAgente
+) {
+  const resposta = await axios.post('http://localhost:8080/pessoa/adicionar', {
     nome: nomeAgente,
     cpf: cpfAgente,
     rg: rgAgente,
-  });
+  })
 
-  let idPessoa = resposta.data.id;
+  let idPessoa = resposta.data.id
 
-  const resposta1 = await axios.post("http://localhost:8080/agente/adicionar", {
+  const resposta1 = await axios.post('http://localhost:8080/agente/adicionar', {
     pessoa: {
       id: idPessoa,
     },
     departamento: departamentoAgente,
-  });
+  })
 }
 
-
 function openAgenteModal() {
-  let agenteMod = document.querySelector(".contain-form-modal");
-  let backGroudModal = document.querySelector(".background-open-modalAgente");
+  let agenteMod = document.querySelector('.contain-form-modal')
+  let backGroudModal = document.querySelector('.background-open-modalAgente')
 
-  if (agenteMod.style.display == "none" || agenteMod.style.display == "") {
-    agenteMod.style.display = "flex";
-    backGroudModal.style.display = "block";
+  if (agenteMod.style.display == 'none' || agenteMod.style.display == '') {
+    agenteMod.style.display = 'flex'
+    backGroudModal.style.display = 'block'
+    document.body.style.overflowY = 'hidden'
   } else {
-    backGroudModal.style.display = "none";
-    agenteMod.style.display = "none";
+    backGroudModal.style.display = 'none'
+    agenteMod.style.display = 'none'
+    document.body.style.overflowY = 'scroll'
   }
 }
 
 function openOcorrenciaModal() {
-  let ocorrenciaMod = document.querySelector(".contain-form-modal-ocorrencia");
-  let backGroudModal = document.querySelector(".background-open-modalOcorrencia");
+  let ocorrenciaMod = document.querySelector('.contain-form-modal-ocorrencia')
+  let backGroudModal = document.querySelector(
+    '.background-open-modalOcorrencia'
+  )
 
-  if (ocorrenciaMod.style.display == "none" || ocorrenciaMod.style.display == "") {
-    ocorrenciaMod.style.display = "flex";
-    backGroudModal.style.display = "block";
+  if (
+    ocorrenciaMod.style.display == 'none' ||
+    ocorrenciaMod.style.display == ''
+  ) {
+    ocorrenciaMod.style.display = 'flex'
+    backGroudModal.style.display = 'block'
+    document.body.style.overflowY = 'hidden'
   } else {
-    backGroudModal.style.display = "none";
-    ocorrenciaMod.style.display = "none";
+    backGroudModal.style.display = 'none'
+    ocorrenciaMod.style.display = 'none'
+    document.body.style.overflowY = 'scroll'
   }
 }
 
-
 function openListAgenteEmOcorrencia() {
-  const selectAgenteMod = document.querySelector(".contain-form-modal-lista-select");
-  const backGroudModal = document.querySelector(".background-open-modalListaAgenteSelect");
-  const selectAgente = document.getElementById("modalAgente");
+  const selectAgenteMod = document.querySelector(
+    '.contain-form-modal-lista-select'
+  )
+  const backGroudModal = document.querySelector(
+    '.background-open-modalListaAgenteSelect'
+  )
+  const selectAgente = document.getElementById('modalAgente')
 
-  document.body.style.overflowY = "hidden"
   // Garante que nenhum outro modal esteja visível
-  selectAgente.style.display = "none"; // Fecha o modal de agentes se estiver aberto
-  selectAgenteMod.style.display = "none"; // Fecha qualquer outra lista
-  backGroudModal.style.display = "block"; // Fecha o fundo escuro
+  selectAgente.style.display = 'none' // Fecha o modal de agentes se estiver aberto
+  selectAgenteMod.style.display = 'none' // Fecha qualquer outra lista
+  backGroudModal.style.display = 'block' // Fecha o fundo escuro
 
   // Agora abre o modal de seleção de agentes
-  selectAgenteMod.style.display = "block";
-  backGroudModal.style.display = "none";
+  selectAgenteMod.style.display = 'block'
+  backGroudModal.style.display = 'none'
 
   // Chama a função para listar os agentes
-  listAgenteEmOcorrencia();
+  listAgenteEmOcorrencia()
 }
 
 async function listAgenteEmOcorrencia() {
-  const modal = document.getElementById("modalAgente");
-  const tabela = document.querySelector("#tabelaAgentes tbody");
+  const modal = document.getElementById('modalAgente')
+  let tbody = document.querySelector('#tabelaAgentes tbody')
 
-  // Limpa a tabela antes de adicionar os agentes
-  tabela.innerHTML = "";
+  tbody.innerHTML = ''
 
-  // Faz a requisição para obter a lista de agentes
-  const response = await axios.get("http://127.0.0.1:8080/agente/listar");
-  const agentes = response.data;
+  const response = await axios.get('http://10.110.12.40:8080/agente/listar')
+  const agentes = response.data
 
-  // Preenche a tabela com os agentes
   agentes.forEach((agente) => {
-    const row = document.createElement("tr");
-    row.innerHTML = `
-      <td>${agente.id}</td>
-      <td>${agente.pessoa.nome}</td>
-      <td>${agente.departamento}</td>
-      <td>
-        <button onclick="selecionarAgente(${agente.id}, '${agente.pessoa.nome}')">
-          Selecionar
-        </button>
-      </td>
-    `;
-    tabela.appendChild(row);
-  });
+    tbody.innerHTML += `
+      <tr onClick="selecionarAgente(this)">
+        <td>${agente.id}</td>
+        <td>${agente.pessoa.nome}</td>
+        <td>${agente.departamento}</td>
+      </tr>
+    `
+  })
 
-  // Exibe o modal
-  modal.style.display = "block";
+  modal.style.display = 'block'
 }
 
+function selecionarAgente(tr) {
+  const buttonAddAgente = document.querySelector('.btn-agent-Ocorrencia')
+  console.log(buttonAddAgente)
+  buttonAddAgente.setAttribute(
+    'data-id',
+    tr.querySelectorAll('td')[0].innerHTML
+  )
 
-// Vincular o agente selecionado ao formulário de ocorrência
-function selecionarAgente(id, nome) {
-  // Exibe o agente selecionado no formulário
-  const agenteSelecionado = document.getElementById("agenteSelecionado");
-  agenteSelecionado.textContent = `Agente Selecionado: ${nome}`;
-  agenteSelecionado.dataset.id = id;
+  buttonAddAgente.innerText = `Agente Selecionado: ${
+    tr.querySelectorAll('td')[1].innerText
+  }`
 
-  // Fecha o modal
-  fecharModalAgente();
+  fecharModalAgente()
 }
 
 // Fechar o modal de seleção de agentes
 function fecharModalAgente() {
-  const modal = document.getElementById("modalAgente");
-  modal.style.display = "none";
-  const listaAgenteMod = document.querySelector(".contain-form-modal-lista-select");
-  listaAgenteMod.style.display = "none";
-  const backGroudModal = document.querySelector(".background-open-modalListaAgente");
-  backGroudModal.style.display = "none";
+  const modal = document.getElementById('modalAgente')
+  modal.style.display = 'none'
+  const listaAgenteMod = document.querySelector(
+    '.contain-form-modal-lista-select'
+  )
+  listaAgenteMod.style.display = 'none'
+  const backGroudModal = document.querySelector(
+    '.background-open-modalListaAgente'
+  )
+  backGroudModal.style.display = 'none'
 }
 
-
 function openListaAgenteModal() {
-  const listaAgenteMod = document.querySelector(".contain-form-modal-lista");
-  const backGroudModal = document.querySelector(".background-open-modalListaAgente");
+  const listaAgenteMod = document.querySelector('.contain-form-modal-lista')
+  const backGroudModal = document.querySelector(
+    '.background-open-modalListaAgente'
+  )
 
-  if (listaAgenteMod.style.display === "none" || listaAgenteMod.style.display === "") {
-    listaAgenteMod.style.display = "block"; // Mostra o modal
-    backGroudModal.style.display = "block"; // Ativa o fundo escuro
-    listarAgentes(); // chama a função para listar os agentes
+  if (
+    listaAgenteMod.style.display === 'none' ||
+    listaAgenteMod.style.display === ''
+  ) {
+    listaAgenteMod.style.display = 'block' // Mostra o modal
+    backGroudModal.style.display = 'block' // Ativa o fundo escuro
+    document.body.style.overflowY = 'hidden'
+    listarAgentes() // chama a função para listar os agentes
   } else {
-    listaAgenteMod.style.display = "none"; // Esconde o modal
-    backGroudModal.style.display = "none"; // Esconde o fundo escuro
+    listaAgenteMod.style.display = 'none' // Esconde o modal
+    backGroudModal.style.display = 'none' // Esconde o fundo escuro
+    document.body.style.overflowY = 'scroll'
   }
 }
 
 async function listarAgentes() {
+  const response = await axios.get('http://localhost:8080/agente/listar')
+  const agentes = response.data
 
-  const response = await axios.get("http://localhost:8080/agente/listar");
-  const agentes = response.data;
-
-  const tabelaCorpo = document.querySelector("#tabela-corpo");
-  tabelaCorpo.innerHTML = ""; // Limpa a tabela antes de adicionar novos dados SE TIRAR DÁ RUIM
+  const tabelaCorpo = document.querySelector('#tabela-corpo')
+  tabelaCorpo.innerHTML = ''
 
   agentes.forEach((agente) => {
-    const { pessoa, departamento } = agente;
+    const { pessoa, departamento } = agente
 
     // Criação das linhas da tabela
     const linhaHTML = `
@@ -242,11 +258,9 @@ async function listarAgentes() {
         <td>${pessoa.cpf}</td>
         <td>${departamento}</td>
       </tr>
-    `;
+    `
     //adicionar mais um <td>button para deletear os agents</td>
 
-    tabelaCorpo.innerHTML += linhaHTML; // Adiciona a linha à tabela
-  });
-
+    tabelaCorpo.innerHTML += linhaHTML // Adiciona a linha à tabela
+  })
 }
-
